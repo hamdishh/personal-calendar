f18175d333eb3d6460bdb431018477a4e9080aaf
Hamdi <hamdishh2003@gmail.com>
2024-02-14 17:15:45 +0000

    placed style.css and script.js into assets folder

--- assets/script.js
+++ assets/script.js
@@ -20,23 +20,23 @@ function nowTime () {
 
     $(".time-block").each(function () { //targetting all html elements with time-block class
     var target = $(this); //$(this), refers to the current time-block element being processed and wraps the element in a jQuery object and assigns it to the variable 'select'
-    var id = select.attr("id"); //line retrieves the 'id' attribute of the current .time-block element using jQuery's '.attr() method and assigns it to the 'id' variabkle
+    var id = target.attr("id"); //line retrieves the 'id' attribute of the current .time-block element using jQuery's '.attr() method and assigns it to the 'id' variabkle
     var timeId = id.split("m"); //uses split function to split the 'id' string at the character "m" and stores the resulting array in the variable timeId. This is because the id attr has a format of am0800 or pm1200 that need to be differentiated/easier to tell apart afternoon and morning times
     var currentBlock = parseInt(timeId[1]);// line will extract the second index of id array (part after "m" character) and will convert it into an integer as this part will represent the hour, e.g 0800, 0900 etc.
     var nowFormat = now + "00"; //concatenates variable "now" with the string"00" 
 
-    if (currentBlock == nowFormat) {
-        $(this).addClass("past");
-        $(this).addClass("present");
-        $(this).addClass("future");
+ if (currentBlock == nowFormat) {
+    target.addClass("past");
+    target.addClass("present");
+    target.addClass("future");
     } else if (currentBlock > nowFormat) {
-        $(this).addClass("future");
-        $(this).addClass("past");
-        $(this).addClass("present");
+        target.addClass("future");
+        target.addClass("past");
+        target.addClass("present");
     } else {
-        $(this).addClass("present");
-        $(this).addClass("future");
-        $(this).addClass("past");
+        target.addClass("present");
+        target.addClass("future");
+        target.addClass("past");
     }
 
 });
